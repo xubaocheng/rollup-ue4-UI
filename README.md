0445d19e24df02b8121b26b95db17ba79e6e86e4
Bates <xth5516739@163.com>
2021-07-22 21:39:32 +0800

    配置开发 打包 文件

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # full-screen-ue4-UI
 
-
-
 # 简介
 数据大屏UI组件库 to ue4
 
 ### 安装组件库
 ```
-npm i datav-libs-dev
 ```
-
 ## 运行项目
 ```
 npm install
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // es6只有export可以触发tree shaking机制。commonjs只有exports可以触发tree shaking机制
 import Test from './components/Test/index'
 
+import VueEcharts from './components/VueEcharts/index'
+
 export default function(Vue) {
   Vue.use(Test)
+  Vue.use(VueEcharts)
 }
